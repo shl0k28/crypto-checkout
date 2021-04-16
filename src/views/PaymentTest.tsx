0d60f6ecf3512193a6fcf69b8c5faf3e15e8062e
Shlok D. Mange <shlok_mange@protonmail.com>
2021-04-16 23:32:20 +0400

    Eth Price API

--- src/views/PaymentTest.tsx
+++ src/views/PaymentTest.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
 //Web3 stuff
 import Web3Modal from 'web3modal'
 import Web3 from 'web3'
 import Portis from '@portis/web3'
+
+var coinApi = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=2&page=1&sparkline=false`
 
 const PaymentTest: React.FC = () => {
 	
@@ -36,6 +39,7 @@ const PaymentTest: React.FC = () => {
 	}
 
 	return(
+		<>
 		<div style={{fontFamily:"'Quicksand', sans-serif"}} className="bg-gray-200 h-screen w-screen lg:space-y-8">
 			<nav className="px-8 py-4 lg:px-16">
 				<NavLink to="/" style={{fontFamily:"'Krub', sans-serif"}} className="text-3xl">axios</NavLink>
@@ -47,16 +51,80 @@ const PaymentTest: React.FC = () => {
 			<div className="px-8 py-4 lg:px-16">
 				<button onClick={connectToPortis} className="bg-gray-800 p-2 text-gray-200">Checkout with Crypto</button>
 			</div>
-			{
-				completePayment && <>Transaction Modal</>
-			}
 		</div>
+			{
+				completePayment && <TransactionModal />
+			}
+		</>
 	)
 }
 
 const TransactionModal: React.FC = () => {
+	
+	const [priceInEth, setPriceInEth] = React.useState<number | null>(null)
+	const [loading, setLoading] = React.useState(true)
+
+	const amount = 396.60
+
+	React.useEffect(() => {
+		getEthPrice()
+	},[])
+	
+	const getEthPrice = async () => {
+		var res = await axios.get(coinApi)
+		var ethPrice = res.data[1].current_price
+		console.log(ethPrice)
+		var price = parseFloat((amount/ethPrice).toFixed(3))
+		console.log(price)
+		setPriceInEth(price)
+		setLoading(false)
+	}
+
 	return(
-		<></>
+		<>
+			<div
+            	className="justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none"
+          	>
+            <div className="relative w-auto mx-auto max-w-md">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Finish Payment
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    Total order : <span className="text-blueGray-800 font-semibold"> ${amount} </span> 
+                  </p>
+				  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    Amount in ETH: <span className="text-blueGray-800 font-semibold"> {priceInEth} </span> 
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => {}}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
 	)
 }
 
